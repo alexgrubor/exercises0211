c31462fb6353d82060b220cb4b577d8cc7113c96
Aleksandra G <86660025+alexgrubor@users.noreply.github.com>
2022-11-03 08:18:24 +0100

    ex6 made with filter methode

--- code6.js
+++ code6.js
@@ -1,6 +1,8 @@
 //Remove duplicates of an array and return an array of only unique elements  
 let array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
-let newArray =[];
+let newArray =array.filter ((item, index) => {
+    return array.indexOf(item) == index
+})
 
 
-console.log(some)
+console.log(newArray)
